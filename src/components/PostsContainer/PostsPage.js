a0bedcd7a51c887733bc182ea817572031e07ac8
Eorium <travisgent312@gmail.com>
2020-06-09 16:53:39 -0600

    still wip. Add basic intro stuff

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,21 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+import { useState } from "react";
+import "../../dummy-data.js";
+import dummyData from "../../dummy-data.js";
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+
+  const [posts, setPosts] = useState(dummyData);
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        <div className="post-thumb">{dummyData.thumbnailUrl}</div>
+      }
     </div>
   );
 };
